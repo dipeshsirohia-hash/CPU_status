5384b00618b374d94f51a7eb9d96dfb5515deac4
Dipesh Sirohia <Dipesh.Sirohia@in2ittech.com>
2026-03-12 18:07:54 +0530

    line 7

--- login/Authentication.go
+++ login/Authentication.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("This is the Authentication module.")
-	fmt.Println("This is the Authentication module.")
+	fmt.Println("This is the Authentication module. change in line 7")
 	fmt.Println("This is the Authentication module.")
 	fmt.Println("This is the Authentication module.")
 	fmt.Println("This is the Authentication module.")
