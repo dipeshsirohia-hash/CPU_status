938820b47e8259daad4fba438cdcfeebfedbb598
Dipesh Sirohia <Dipesh.Sirohia@in2ittech.com>
2026-03-13 11:11:45 +0530

    user code

--- login/user.go
+++ login/user.go
@@ -1,1 +1,13 @@
 package login
+
+import "fmt"
+
+
+func User() {
+	fmt.Println("code by dipesh")
+	fmt.Println("This is the customer module.")
+	fmt.Println("This is the customer module.")
+	fmt.Println("This is the customer module.")
+	fmt.Println("This is the customer module.")
+	fmt.Println("This is the customer module.")		
+}
