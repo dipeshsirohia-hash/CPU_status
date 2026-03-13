74a4fe32a4fef89d68034325cd3bde71c52354b1
sandeep singh <sandeep.singh@in2ittech.com>
2026-03-13 11:57:59 +0530

    n bmvbnbv

--- login/customer.go
+++ login/customer.go
@@ -5,4 +5,6 @@ func Customer()  {
 	fmt.Println("sandeep working in customer.go")
 	fmt.Println("New line added by dipesh")
 	
+	fmt.Println("sandeep add new line no.1")
+	fmt.Println("sandeep add new line no.2")
 }
